7e29d46e21594ba785fbfc1a2165e6160f0740ee
lucas-mateus <xlucasmat@gmail.com>
2023-03-04 16:41:52 -0300

    feat: configure global styles and default colors

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,24 @@
+export default {
+  colors: {
+    backgroundColor: '#F6F5FC',
+    primary: {
+      main: '#5061FC',
+      light: '#6674F4',
+      dark: '#3346f0',
+      lighter: '#e0e3ff',
+    },
+    gray: {
+      900: '#222222',
+      200: '#BCBCBC',
+      100: '#E6E6E6',
+    },
+    danger: {
+      main: '#FC5050',
+      light: '#F97171',
+      dark: '#F63131',
+    },
+    success: {
+      main: '#51CA73',
+    },
+  },
+};
