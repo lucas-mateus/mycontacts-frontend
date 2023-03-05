807d0f3db340138d4a98922f7dca2b4590140d85
lucas-mateus <xlucasmat@gmail.com>
2023-03-05 00:15:34 -0300

    fix: remove search bar from the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,10 @@
 import logo from '../../assets/images/logo.svg';
-import { Container, InputSearchContainer } from './styles';
+import { Container } from './styles';
 
 export function Header() {
   return (
     <Container>
       <img src={logo} alt="my contacts" width={200} />
-      <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato..." />
-      </InputSearchContainer>
     </Container>
   );
 }
