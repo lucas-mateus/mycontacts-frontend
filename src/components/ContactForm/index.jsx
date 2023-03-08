f46f1289eadf34f78a9c85b9771b8dd7b953fae0
lucas-mateus <xlucasmat@gmail.com>
2023-03-08 00:07:12 -0300

    fix: transform the form into a Controlled Component

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { FormGroup } from '../FormGroup';
 import { Form } from './styles';
 import { Input } from '../Input';
@@ -6,30 +7,58 @@ import { Select } from '../Select';
 import { Button } from '../Button';
 
 export function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <Form>
+    <Form onSubmit={(event) => handleSubmit(event)}>
       <FormGroup>
-        <Input placeholder="Nome..." type="text" />
+        <Input
+          placeholder="Nome..."
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup error="O formato do e-mail é inválido.">
+        <Input
+          placeholder="E-mail..."
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="E-mail..." type="email" />
+        <Input
+          placeholder="Telefone..."
+          type="tel"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone..." type="tel" />
-      </FormGroup>
-
-      <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
           <option value="" disabled>
-            Selecione uma categoria
+            Categoria
           </option>
 
           <option value="instagram">Instagram</option>
-          <option>LinkedIn</option>
-          <option>Facebook</option>
-          <option>WhatsApp</option>
+          <option value="discord">Discord</option>
+          <option value="linkedin">LinkedIn</option>
+          <option value="facebook">Facebook</option>
+          <option value="whatsapp">WhatsApp</option>
         </Select>
       </FormGroup>
 
