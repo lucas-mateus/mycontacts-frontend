9d1b6a64dd05ebaffc828249e1b6b55374ee7038
lucas-mateus <xlucasmat@gmail.com>
2023-03-12 12:28:41 -0300

    fix: set appearence none  in mobile devices

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -9,7 +9,7 @@ export const Select = styled.select`
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   border-radius: 4px;
   outline: none;
-
+  appearance: none;
   font-size: 16px;
 
   transition: border 0.4s ease-in;
