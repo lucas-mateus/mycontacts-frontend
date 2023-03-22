3cddf369d59efcc0cd19cfe4c44ae93a63736331
lucas-mateus <xlucasmat@gmail.com>
2023-03-22 15:50:46 -0300

    feat: make the http request to server and render list

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,34 +1,69 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import arrow from '../../assets/images/arrow.svg';
-import { Modal } from '../../components/Modal';
+import { ContactCard } from '../../components/ContactCard';
 import {
   Container,
   Header,
-  ListContainer,
+  ListHeader,
   InputSearchContainer,
+  CardContainer,
 } from './styles';
 
 export function Home() {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('ASC');
+
+  function handleToggleOrderBy() {
+    setOrderBy((prevOrder) => (prevOrder === 'ASC' ? 'DESC' : 'ASC'));
+  }
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`, {
+      method: 'GET',
+    })
+      .then(async (response) => {
+        const requestedContacts = await response.json();
+        setContacts(requestedContacts);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [orderBy]);
+
   return (
     <Container>
-      <Modal danger />
-
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato..." />
       </InputSearchContainer>
       <Header>
-        <h2>3 contatos</h2>
+        <h2>
+          {contacts.length === 1
+            ? `${contacts.length} contato`
+            : `${contacts.length} contatos`}
+        </h2>
         <Link to="/new">Novo Contato</Link>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type="button">
-            <span> Nome</span>
-            <img src={arrow} alt="arrow" />
-          </button>
-        </header>
-      </ListContainer>
+      <ListHeader orderBy={orderBy}>
+        <button type="button" onClick={handleToggleOrderBy}>
+          <span> Nome</span>
+          <img src={arrow} alt="arrow" />
+        </button>
+      </ListHeader>
+
+      <CardContainer>
+        {contacts.map((contact) => (
+          <ContactCard
+            key={contact.id}
+            id={contact.id}
+            name={contact.name}
+            categoryName={contact.category_name}
+            email={contact.email}
+            phone={contact.phone}
+          />
+        ))}
+      </CardContainer>
     </Container>
   );
 }
