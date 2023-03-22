d355397e677ff3668542070a3ae0f993ba7f83d8
lucas-mateus <xlucasmat@gmail.com>
2023-03-22 19:03:08 -0300

    feat: create the search function and loading delay

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import arrow from '../../assets/images/arrow.svg';
 import { ContactCard } from '../../components/ContactCard';
@@ -10,37 +10,65 @@ import {
   CardContainer,
 } from './styles';
 
+import { Loader } from '../../components/Loader';
+import delay from '../../utils/delay';
+
 export function Home() {
   const [contacts, setContacts] = useState([]);
+  const [isLoad, setIsLoad] = useState(true);
   const [orderBy, setOrderBy] = useState('ASC');
+  const [searchTerm, setSearchTerm] = useState('');
 
   function handleToggleOrderBy() {
     setOrderBy((prevOrder) => (prevOrder === 'ASC' ? 'DESC' : 'ASC'));
   }
 
+  const filteredContacts = useMemo(
+    () =>
+      contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+    [contacts, searchTerm]
+  );
+
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
   useEffect(() => {
+    setIsLoad(true);
     fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`, {
       method: 'GET',
     })
       .then(async (response) => {
+        await delay(800);
         const requestedContacts = await response.json();
         setContacts(requestedContacts);
       })
       .catch((error) => {
         console.log(error);
+      })
+      .finally(() => {
+        setIsLoad(false);
       });
   }, [orderBy]);
 
   return (
     <Container>
+      <Loader isLoad={isLoad} />
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato..." />
+        <input
+          value={searchTerm}
+          type="text"
+          placeholder="Pesquisar contato..."
+          onChange={handleChangeSearchTerm}
+        />
       </InputSearchContainer>
       <Header>
         <h2>
-          {contacts.length === 1
-            ? `${contacts.length} contato`
-            : `${contacts.length} contatos`}
+          {filteredContacts.length === 1
+            ? `${filteredContacts.length} contato`
+            : `${filteredContacts.length} contatos`}
         </h2>
         <Link to="/new">Novo Contato</Link>
       </Header>
@@ -53,7 +81,7 @@ export function Home() {
       </ListHeader>
 
       <CardContainer>
-        {contacts.map((contact) => (
+        {filteredContacts.map((contact) => (
           <ContactCard
             key={contact.id}
             id={contact.id}
